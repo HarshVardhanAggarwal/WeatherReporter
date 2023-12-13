10b95df628d79e69c29ff5fc01c2cfada0969aed
Harsh Vardhan Aggarwal <123395389+HarshVardhanAggarwal@users.noreply.github.com>
2023-12-14 00:43:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+const bodyParser = require("body-Parser");
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+})
+app.post("/", function(req, res) {
+  const query = req.body.cityName;
+  const apiKey = "34da202c0ce8104c978c0ad0127f7267";
+  const unit = "metric"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+  https.get(url, function(response) {
+    console.log(response.statusCode);
+
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+      res.write("<h1>the temperature in " + query + " is" + temp + "degree celcius<h1>");
+      res.write("<p>the weather is curently" + weatherDescription + "</p>");
+      res.write("<img src=" + imageUrl + ">");
+      res.send();
+    })
+  })
+})
+
+
+app.listen("3000", function() {
+  console.log("its running");
+})
